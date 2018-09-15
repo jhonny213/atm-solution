f34f59cc03da13dd55e818c4bbbbadf47341873a
Nacereddine Haddad <haddadnacereddine@gmail.com>
2018-09-15 15:59:50 +0100

    update code ATM to function withdraw(b,r) 	modified:   atm.py

--- atm.py
+++ atm.py
@@ -1,20 +1,24 @@
-money = 2000
-request = 1918
-papers = [200,100,50,20,10,5,2,1]
-if request <= money and request > 0:
-    result = 0
-    for i in papers:
-        j = 0
-        while j < request // i: 
-            print ("give ", i)
-            result += i
-            j = j + 1
-        request = request - i * (request // i)
-# verification
-    print(' Total = ',result)
-
-else:
-    if request > money:
-        print('Error !!! Max request = ', money)
+def withdraw(balance, request):
+    papers = [200,100,50,20,10,5,2,1]
+    if request <= balance and request > 0:
+        whileRequest = request
+        for i in papers:
+            j = 0
+            while j < whileRequest // i:
+                print ("give ", i)
+                j = j + 1
+            whileRequest = whileRequest - i * (whileRequest // i)
+        print balance - request
+        return balance - request
     else:
-        print('Invalid Request ', request , ' !!!')
+        if request > balance:
+            print 'Error !!! Max request = ', balance
+        else:
+            print 'Invalid Request ', request , ' !!!'
+#test  withdraw(balance,request)
+balance = 500
+balance = withdraw(balance, 277)
+balance = withdraw(balance, 30)
+balance = withdraw(balance, 5)
+
+
