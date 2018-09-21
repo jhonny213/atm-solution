24ef325bc6375b73df50383bdc8dceef84830b59
Nacereddine Haddad <haddadnacereddine@gmail.com>
2018-09-21 20:24:10 +0100

    add def show_withdrawals 	modified:   atm.py

--- atm.py
+++ atm.py
@@ -2,6 +2,15 @@ class ATM:
     def __init__(self, balance, bank_name):
         self.balance = balance
         self.bank_name = bank_name
+        self.withdrawals_list = []
+
+    def show_withdrawals(self):
+        for index_withdrawal, withdrawal in enumerate(self.withdrawals_list):
+            print("The withdrawal process " + str(index_withdrawal + 1) + ": has been withdrawn " + str(withdrawal) + " USD")
+        print("==========================================")
+        print("Total withdrawals " + str(sum(self.withdrawals_list)))
+        print("==========================================")
+        print()
 
     def give_p_monny(self, request):
         papers = [200, 100, 50, 20, 10, 5, 2, 1]
@@ -9,19 +18,20 @@ class ATM:
         for i in papers:
             j = 0
             while j < while_request // i:
-                print("give " + str(i))
+                print("give " + str(i) + " USD")
                 j = j + 1
             while_request = while_request - i * (while_request // i)
 
     def withdraw(self, request):
+        lines = "=========================================="
         print("Welcome to " + str(self.bank_name))
         print("Current balance = " + str(self.balance))
-        lines = "=========================================="
         print(lines)
         if self.balance >= request > 0:
             self.give_p_monny(request)
             self.balance -= request
             print(lines)
+            self.withdrawals_list.append(request)
             return self.balance
         else:
             if request > self.balance:
@@ -33,6 +43,7 @@ class ATM:
                 print(lines)
 
 """test class ATM"""
+
 balance1 = 500
 balance2 = 1000
 balance3 = 20000
@@ -43,11 +54,14 @@ atm3 = ATM(balance3, "Tasleef Bank")
 
 atm1.withdraw(277)
 atm1.withdraw(147)
+atm1.show_withdrawals()
 
 atm2.withdraw(100)
 atm2.withdraw(2000)
+atm2.show_withdrawals()
 
 atm3.withdraw(456)
 atm3.withdraw(0)
 atm3.withdraw(20545)
 atm3.withdraw(1000)
+atm3.show_withdrawals()
